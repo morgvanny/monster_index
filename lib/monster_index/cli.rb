db18bb53867fbbdae7cb5f4cf04cfe96383759f4
Morgan VanYperen <morgvanny@gmail.com>
2016-01-24 01:14:22 +0000

    Everything but scraper

--- lib/monster_index/cli.rb
+++ lib/monster_index/cli.rb
@@ -1,5 +1,39 @@
 class MonsterIndex::CLI
+
   def call
-    puts "Welcome to Monster Index"
+    start
   end
+
+  def start
+    puts "Welcome to Monster Index!"
+    puts "Loading monsters..."
+    make_monsters
+    options
+  end
+
+  def make_monsters
+    monster_array = MonsterIndex::Scraper.scrape_index_page
+    MonsterIndex::Monster.create_from_collection(monster_array)
+    MonsterIndex::Monster.all.each do |monster|
+      attributes_hash = MonsterIndex::Scraper.scrape_monster_page(monster.url)
+      monster.add_monster_attributes(attributes_hash)
+    end
+  end
+
+  def options
+    input = ""
+    while input != "exit"
+      puts "Type list to see them all, type any letter to see the monsters starting"
+      puts "with that letter, type the name of a monster to see details, or type exit."
+      input = gets.strip
+      if input.downcase == "list"
+        MonsterIndex::Monster.list
+      elsif input.match(/\A[a-zA-Z]\z/)
+        MonsterIndex::Monster.list_some(input)
+      elsif MonsterIndex::Monster.get_names.any? {|name| name == input}
+        MonsterIndex::Monster.view_profile(input)
+      end
+    end
+  end
+
 end
--- lib/monster_index/monster.rb
+++ lib/monster_index/monster.rb
@@ -1,9 +1,9 @@
 class MonsterIndex::Monster
-  attr_accessors :name, :bio, :attack, :hp, :alignment, :initiative, :speed, :ac, :size_type
+  attr_accessor :name, :bio, :size_type, :hit_dice, :initiative, :speed, :ac, :attack, :alignment, :url
 
   @@all = []
 
-  def initialize(monster_hash)
+  def initialize(monster_hash = {})
     monster_hash.each {|key, value| self.send(("#{key}="), value)}
     @@all << self
   end
@@ -14,14 +14,48 @@ class MonsterIndex::Monster
     end
   end
 
+  def add_monster_attributes(attributes_hash)
+    attributes_hash.each {|key, value| self.send(("#{key}="), value)}
+    self
+  end
+
+  def self.get_names
+    names = []
+    @@all.each do |monster|
+      names << monster.name
+    end
+    names
+  end
+
   def self.list
     @@all.each do |monster|
       puts "#{monster.name}"
     end
   end
 
-  def view_profile
+  def self.list_some(letter)
+    @@all.each do |monster|
+      if monster.name.downcase.match(/\A[#{letter.downcase}]/)
+        puts "#{monster.name}"
+      end
+    end
+  end
 
+  def self.view_profile(name)
+    puts "Description:"
+    puts "#{bio}\n"
+    puts "Size/Type: #{size_type}"
+    puts "Hit Dice: #{hit_dice}"
+    puts "Initiative: #{initiative}"
+    puts "Speed: #{speed}"
+    puts "AC: #{ac}"
+    puts "Attack: #{attack}"
+    puts "Alignment: #{alignment}"
+    puts "More info at: #{url}"
+  end
+
+  def self.all
+    @@all
   end
 
 end
